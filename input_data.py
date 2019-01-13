9459c7b3aed6274644485b64d2a7022630c91418
Shen Zhuoran <cmsflash99@gmail.com>
2019-01-13 17:22:22 +0800

    Refactor

--- input_data.py
+++ input_data.py
@@ -1,77 +1,67 @@
 import numpy
 from collections import deque
+
+
 numpy.random.seed(12345)
 
 
 class InputData:
-    """Store data for word2vec, such as word map, sampling table and so on.
 
-    Attributes:
-        word_frequency: Count of each word, used for filtering low-frequency words and sampling table
-        word2id: Map from word to word id, without low-frequency words.
-        id2word: Map from word id to word, without low-frequency words.
-        sentence_count: Sentence count in files.
-        word_count: Word count in files, without low-frequency words.
-    """
-
-    def __init__(self, file_name, min_count):
-        self.input_file_name = file_name
+    def __init__(self, path, min_count):
+        self.input_path = path
         self.get_words(min_count)
         self.word_pair_catch = deque()
         self.init_sample_table()
-        print('Word Count: %d' % len(self.word2id))
-        print('Sentence Length: %d' % (self.sentence_length))
 
     def get_words(self, min_count):
-        self.input_file = open(self.input_file_name)
+        self.input_file = open(self.input_path)
         self.sentence_length = 0
         self.sentence_count = 0
-        word_frequency = dict()
+        frequencies = dict()
         for line in self.input_file:
             self.sentence_count += 1
             line = line.strip().split(' ')
             self.sentence_length += len(line)
             for w in line:
                 try:
-                    word_frequency[w] += 1
+                    frequencies[w] += 1
                 except:
-                    word_frequency[w] = 1
-        self.word2id = dict()
-        self.id2word = dict()
+                    frequencies[w] = 1
+        self.id_from_word = dict()
+        self.word_from_id = dict()
         wid = 0
-        self.word_frequency = dict()
-        for w, c in word_frequency.items():
+        self.frequencies = dict()
+        for w, c in frequencies.items():
             if c < min_count:
                 self.sentence_length -= c
                 continue
-            self.word2id[w] = wid
-            self.id2word[wid] = w
-            self.word_frequency[wid] = c
+            self.id_from_word[w] = wid
+            self.word_from_id[wid] = w
+            self.frequencies[wid] = c
             wid += 1
-        self.word_count = len(self.word2id)
+        self.word_count = len(self.id_from_word)
 
     def init_sample_table(self):
         self.sample_table = []
         sample_table_size = 1e8
-        pow_frequency = numpy.array(list(self.word_frequency.values()))**0.75
-        words_pow = sum(pow_frequency)
-        ratio = pow_frequency / words_pow
+        pow_frequencies = numpy.array(list(self.frequencies.values()))**0.75
+        words_pow = sum(pow_frequencies)
+        ratio = pow_frequencies / words_pow
         count = numpy.round(ratio * sample_table_size)
         for wid, c in enumerate(count):
             self.sample_table += [wid] * int(c)
         self.sample_table = numpy.array(self.sample_table)
 
-    # @profile
     def get_batch_pairs(self, batch_size, window_size):
         while len(self.word_pair_catch) < batch_size:
             sentence = self.input_file.readline()
             if sentence is None or sentence == '':
-                self.input_file = open(self.input_file_name)
+                self.input_file = open(self.input_path)
                 sentence = self.input_file.readline()
             word_ids = []
             for word in sentence.strip().split(' '):
                 try:
-                    word_ids.append(self.word2id[word])
+                    word_ids.append(self.id_from_word[word])
                 except:
                     continue
             for i, u in enumerate(word_ids):
@@ -87,20 +77,11 @@ class InputData:
             batch_pairs.append(self.word_pair_catch.popleft())
         return batch_pairs
 
-    # @profile
     def get_neg_v_neg_sampling(self, pos_word_pair, count):
         neg_v = numpy.random.choice(
             self.sample_table, size=(len(pos_word_pair), count)).tolist()
         return neg_v
 
-    def evaluate_pair_count(self, window_size):
+    def get_pair_count(self, window_size):
         return self.sentence_length * (2 * window_size - 1) - (
             self.sentence_count - 1) * (1 + window_size) * window_size
-
-
-def test():
-    a = InputData('./zhihu.txt')
-
-
-if __name__ == '__main__':
-    test()
--- model.py
+++ model.py
@@ -1,77 +1,38 @@
 import torch
-from torch.autograd import Variable
-import torch.nn as nn
-import torch.nn.functional as F
+from torch import nn
+from torch.nn import functional as f
 
 
 class SkipGramModel(nn.Module):
-    """Skip gram model of word2vec.
 
-    Attributes:
-        emb_size: Embedding size.
-        emb_dimention: Embedding dimention, typically from 50 to 500.
-        u_embedding: Embedding for center word.
-        v_embedding: Embedding for neibor words.
-    """
+    def __init__(self, word_count, dimension):
+        super().__init__()
+        self.word_count = word_count
+        self.dimension = dimension
+        self.u_embedding = nn.Embedding(word_count, dimension)
+        self.v_embedding = nn.Embedding(word_count, dimension)
+        self.init_embeddings()
 
-    def __init__(self, emb_size, emb_dimension):
-        """Initialize model parameters.
-
-        Apply for two embedding layers.
-        Initialize layer weight
-
-        Args:
-            emb_size: Embedding size.
-            emb_dimention: Embedding dimention, typically from 50 to 500.
-
-        Returns:
-            None
-        """
-        super(SkipGramModel, self).__init__()
-        self.emb_size = emb_size
-        self.emb_dimension = emb_dimension
-        self.u_embeddings = nn.Embedding(emb_size, emb_dimension, sparse=True)
-        self.v_embeddings = nn.Embedding(emb_size, emb_dimension, sparse=True)
-        self.init_emb()
-
-    def init_emb(self):
-        """Initialize embedding weight like word2vec.
-
-        The u_embedding is a uniform distribution in [-0.5/em_size, 0.5/emb_size], and the elements of v_embedding are zeroes.
-
-        Returns:
-            None
-        """
-        initrange = 0.5 / self.emb_dimension
-        self.u_embeddings.weight.data.uniform_(-initrange, initrange)
-        self.v_embeddings.weight.data.uniform_(-0, 0)
+    def init_embeddings(self):
+        range_ = 0.5 / self.dimension
+        nn.init.uniform_(self.u_embedding.weight, -range_, range_)
+        nn.init.zeros_(self.v_embedding.weight)
 
     def forward(self, pos_u, pos_v, neg_v):
-        """Forward process.
-
-        As pytorch designed, all variables must be batch format, so all input of this method is a list of word id.
-
-        Args:
-            pos_u: list of center word ids for positive word pairs.
-            pos_v: list of neibor word ids for positive word pairs.
-            neg_u: list of center word ids for negative word pairs.
-            neg_v: list of neibor word ids for negative word pairs.
-
-        Returns:
-            Loss of this process, a pytorch variable.
-        """
-        emb_u = self.u_embeddings(pos_u)
-        emb_v = self.v_embeddings(pos_v)
-        score = torch.mul(emb_u, emb_v).squeeze()
+        u_embedding = self.u_embedding(pos_u)
+        v_embedding = self.v_embedding(pos_v)
+        score = torch.mul(u_embedding, v_embedding).squeeze()
         score = torch.sum(score, dim=1)
-        score = F.logsigmoid(score)
-        neg_emb_v = self.v_embeddings(neg_v)
-        neg_score = torch.bmm(neg_emb_v, emb_u.unsqueeze(2)).squeeze()
-        neg_score = F.logsigmoid(-1 * neg_score)
+        score = f.logsigmoid(score)
+        neg_v_embedding = self.v_embedding(neg_v)
+        neg_score = torch.bmm(
+            neg_v_embedding, u_embedding.unsqueeze(2)
+        ).squeeze()
+        neg_score = f.logsigmoid(-1 * neg_score)
         return -1 * (torch.mean(score)+torch.mean(neg_score))
 
-    def save_embedding(self, id2word, file_name, use_cuda):
-        """Save all embeddings to file.
+    def save_embedding(self, id2word, file_name):
+        """Save all embedding to file.
 
         As this class only record word id, so the map from id to word has to be transfered from outside.
 
@@ -81,12 +42,9 @@ class SkipGramModel(nn.Module):
         Returns:
             None.
         """
-        if use_cuda:
-            embedding = self.u_embeddings.weight.cpu().data.numpy()
-        else:
-            embedding = self.u_embeddings.weight.data.numpy()
+        embedding = self.u_embedding.weight.cpu().data.numpy()
         fout = open(file_name, 'w')
-        fout.write('%d %d\n' % (len(id2word), self.emb_dimension))
+        fout.write('%d %d\n' % (len(id2word), self.dimension))
         for wid, w in id2word.items():
             e = embedding[wid]
             e = ' '.join(map(lambda x: str(x), e))
