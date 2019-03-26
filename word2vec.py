0ff840bb734260533510f487acd70550f351e3b7
VVVFO <yaoqingning@gmail.com>
2019-03-26 15:05:20 +0800

    Use dirs to save embeddings

--- word2vec.py
+++ word2vec.py
@@ -1,3 +1,4 @@
+import math
 from os import path as osp
 from input_data import InputData
 import numpy
@@ -18,12 +19,12 @@ batch_size = 50
 class Word2Vec:
 
     def __init__(
-            self, input_path, output_path, dimension=100,
-            batch_size=batch_size, window_size=5, epoch_count=10,
+            self, input_path, output_dir, wordsim_path, dimension=100,
+            batch_size=batch_size, window_size=5, epoch_count=1,
             initial_lr=2.5, min_count=5
         ):
         self.data = InputData(input_path, min_count)
-        self.output_path = output_path
+        self.output_dir = output_dir
         self.vocabulary_size = len(self.data.id_from_word)
         self.dimension = dimension
         self.batch_size = batch_size
@@ -38,17 +39,21 @@ class Word2Vec:
         self.model = nn.DataParallel(self.model.to(self.device))
         self.optimizer = optim.SGD(self.model.parameters(), lr=self.initial_lr)
 
-        self.wordsim_verification_tuples = []
-        with open('chinese-297.txt', 'r') as f:
-            f.readline() # Abandon header
-            for line in f:
-                word1, word2, actual_similarity = line.split(',')
-                self.wordsim_verification_tuples.append((word1, word2, float(actual_similarity)))
+        if wordsim_path:
+            self.wordsim_verification_tuples = []
+            with open(wordsim_path, 'r') as f:
+                f.readline() # Abandon header
+                for line in f:
+                    word1, word2, actual_similarity = line.split(',')
+                    self.wordsim_verification_tuples.append((word1, word2, float(actual_similarity)))
+        else:
+            self.wordsim_verification_tuples = None
 
     def train(self):
         pair_count = self.data.get_pair_count(self.window_size)
         batch_count = self.epoch_count * pair_count / self.batch_size
-        for i in range(int(batch_count)):
+        best_rho = 0
+        for i in tqdm(range(int(batch_count)), total=batch_count):
             pos_pairs = self.data.get_batch_pairs(
                 self.batch_size, self.window_size
             )
@@ -65,24 +70,29 @@ class Word2Vec:
             loss.backward()
             self.optimizer.step()
 
-            if i % 100 == 0:
-                print("Loss: %0.8f, lr: %0.6f" %
-                        (loss.item(),
-                        self.optimizer.param_groups[0]['lr']))
+            if i % 10000 == 0:
+                if self.wordsim_verification_tuples:
+                    rho = self.model.module.get_wordsim_rho(
+                        self.wordsim_verification_tuples, self.data.id_from_word
+                    )
+                else:
+                    rho = 0
+                print(f'Loss: {loss.item()}, lr: {self.optimizer.param_groups[0]["lr"]}, rho: {rho}')
+                if rho > best_rho or math.isnan(rho):
+                    dump_embedding(
+                        self.model.module.get_embedding(), self.model.module.dimension,
+                        self.data.word_from_id, osp.join(self.output_dir, f'{i}_{rho}.txt')
+                    )
+                    best_rho = rho
             if i * self.batch_size % 100000 == 0:
                 lr = self.initial_lr * (1.0 - 1.0 * i / batch_count)
                 for param_group in self.optimizer.param_groups:
                     param_group['lr'] = lr
-        dump_embedding(
-            self.model.module.get_embedding(), self.model.module.dimension,
-            self.data.word_from_id, self.output_path
-        )
-        print(f'''Spearman\'s rho={self.model.module.get_wordsim_rho(
-            self.wordsim_verification_tuples, self.data.id_from_word
-        )}''')
 
 
 if __name__ == '__main__':
-    w2v = Word2Vec(input_path=sys.argv[1], output_path=sys.argv[2])
+    if len(sys.argv) == 3:
+        sys.argv.append(None)
+    w2v = Word2Vec(input_path=sys.argv[1], output_dir=sys.argv[2], wordsim_path=sys.argv[3])
     w2v.train()
 
